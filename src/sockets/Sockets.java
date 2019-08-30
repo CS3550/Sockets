7f97e24af507d97f392f05d470ae3f02cf26bc82
Ricks <bricks@unomaha.edu>
2019-08-30 15:58:55 -0500

    Update Sockets.java

--- src/sockets/Sockets.java
+++ src/sockets/Sockets.java
@@ -26,12 +26,15 @@ public class Sockets {
      */
     public static void main(String[] args) {
         try {
+            System.out.println("Started the program.")
             ServerSocket serverSocket = new ServerSocket(5000);
 
             while (true) {
 
                 Socket clientSocket = serverSocket.accept(); //Blocking method call...It won't return until
                 //A client socket connects
+                
+                System.out.println("A client connected.")
 
                 ThreadedSocket threadedSocket = new ThreadedSocket(clientSocket);
                 Thread thread = new Thread(threadedSocket);
